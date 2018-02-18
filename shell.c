0417ed010abd8d1efcedaf2f0d8582ddbdbb9be4
Malikoto <bigbig2540@hotmail.com>
2018-02-18 22:24:00 +0700

    Fixed Indent

--- shell.c
+++ shell.c
@@ -18,13 +18,14 @@ char** commandSpliter(char *inputCommand)
     char **commandList = (char **) malloc(commandLength * sizeof(char *));
     // for seperating each commands with ";"
     while( (command = strsep(&inputCommand,";")) != NULL ){
- if(strlen(command) == 0)continue;
- if (strcmp(command, "exit") == 0 || strcmp(command, "as") == 0) { // we wanna use quit instance of exit
-  fprintf(stderr , "\033[1;31m%s: command not found\n\033[0;m",command);
-                continue;
+
+        if(strlen(command) == 0)continue;
+        if (strcmp(command, "exit") == 0 || strcmp(command, "as") == 0) { // we wanna use quit instance of exit
+            fprintf(stderr , "\033[1;31m%s: command not found\n\033[0;m",command);
+            continue;
         }
         commandList[i] = command;
- i++;
+        i++;
     }
     commandList[i++] = 0;
 
@@ -44,13 +45,14 @@ char** argumentDetect(char *inputCommand)
     char **tokenList = (char **) malloc(commandLength * sizeof(char *));
     // for seperating token with space bar
     while( (token = strsep(&inputCommand," ")) != NULL ){
- if(strcmp(token,"quit") == 0) { //check quit command
-     printf("\033[1;31mExiting Shell...\n");
-     exit(0);
- }
- else if(strlen(token)==0) continue; //in case of intpu = "; ; ;"  
-        if(token != NULL && token[0] != 0 && token[0] != ' '){
-            tokenList[i] = token; 
+
+        if(strcmp(token,"quit") == 0) { //check quit command
+            printf("\033[1;31mExiting Shell...\n");
+            exit(0);
+        }
+         else if(strlen(token)==0) continue; //in case of intpu = "; ; ;"  
+            if(token != NULL && token[0] != 0 && token[0] != ' '){
+                tokenList[i] = token; 
         } 
         else{
             i--;
@@ -72,62 +74,61 @@ void executeCommand(char **commandList)
 
     while (1) {
 
- command = argumentDetect(commandList[commandCount++]); // seperate command list in to one command 
- 
- if(command == NULL) break;
- 
- // create child process
-        else {
-     pid_t forkArgument = fork();
+        command = argumentDetect(commandList[commandCount++]); // seperate command list in to one command 
+     
+        if(command == NULL) break;
+        else {// create child process
+            pid_t forkArgument = fork();
 
-     if (!forkArgument) { // if pid == 0
-
+            if (!forkArgument) { // if pid == 0
                 int execvpArgument = execvp(command[0], command); // send command to shell 
 
-         if(execvpArgument < 0) {
-      fprintf(stderr , "\033[1;31m%s: command not found\n\033[0;m",command[0]);  
+                if(execvpArgument < 0) {
+                    fprintf(stderr , "\033[1;31m%s: command not found\n\033[0;m",command[0]);  
                 }  
-         // cannot find command in shell
+                // cannot find command in shell
                 exit(0);
-
-  return 0;
+                return 0;
             }
         }
- // wait for each child to finish their process
-     for (i = 0; i < commandCount; i++) 
-     wait(&status);
+        // wait for each child to finish their process
+        for (i = 0; i < commandCount; i++) 
+            wait(&status);
     }
-    
- 
+
     free(commandList); // free memory that input commands allocate
 }
 
 //Handle CTRL+C >> In Shell CTRL+C Do Not Exit The Program
 void sigint_handler(int signal){
- printf("\n\033[1;31mCan Not Terminated Program By Using Ctrl+C\033[0;m\n$ ");
- fflush(stdout);
+    printf("\n\033[1;31mCan Not Terminated Program By Using Ctrl+C\033[0;m\n$ ");
+    fflush(stdout);
 }
 
 int main(int argc , char **argv){
  
     signal(SIGINT,sigint_handler);
 
-    
     //INTERACTIVE MODE
     if(argc == 1){
         printf("\033[1;94mEntering Shell...\n\033[0;m");
+
         while(1){
   
             char inputCommand[inputStringLength];  
+
             inputCommand[0] = '\0'; // for pressing enter with no command
+
             printf("$ ");
             scanf("%[^\n]",inputCommand); // for all input from keyboard with spacebar
             getchar(); // do not want "/n"
-     if(strlen(inputCommand) == 0) continue;
+
+            if(strlen(inputCommand) == 0) continue;
+
             executeCommand(commandSpliter(inputCommand)); // function for sending input to run
- 
         }
     }
+
     //BATCH MODE
     else if(argc==2){
         FILE *fp;   
@@ -151,9 +152,8 @@ int main(int argc , char **argv){
     }
     //In case of more than 1 argv input
     else{
- fprintf(stderr , "\033[1;31mError : Only One Batch File Can Be Excecute\n");
- return 0;
-
+        fprintf(stderr , "\033[1;31mError : Only One Batch File Can Be Excecute\n");
+        return 0;
     }
 
     return 0;
